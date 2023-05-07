62ecd1155b76754b9e036cb4d7ee18d21f4b9c95
CamiloCon21 <florezivancamilo@gmail.com>
2023-05-07 18:56:20 -0500

    a

---
+++ proyecto_Interfaces/src/app/series/series.component.ts
@@ -0,0 +1,65 @@
+import { eventmodel } from '../models/events.model';
+
+import { Service } from './../services/collection';
+import { comics } from '../services/comics';
+
+import { Component, Injectable, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { Router, RouterLink } from '@angular/router';
+import { events } from '../services/events';
+
+
+
+@Component({
+  selector: 'app-series',
+  templateUrl: './series.component.html',
+  styleUrls: ['./series.component.css']
+})
+export class seriesComponent implements OnInit {
+  url: any;
+  character : any[] ;
+  descriptions : any[];
+  path : any[] ;
+  extension : any[] ;
+  extension2 : any[] ;
+  id_comic : any
+
+  constructor(public router: Router, public service: comics, public model:eventmodel ) {
+
+   }
+
+  ngOnInit(): void {
+
+    this.service.llamar().subscribe(data=>{
+
+
+        this.character = data as any[]
+        console.log(data)
+        for (const character of this.character) {
+          let urlHero = character.urls[0].url;
+          console.log(urlHero)
+          console.log(character.thumbnail.path)
+          console.log(character.name)        
+            
+        }
+        
+      }
+        )
+  
+}
+
+  Ajustes(){
+  
+   
+  }
+  play(){
+    
+    
+   }
+
+   logros(){
+//
+   }
+
+
+}
---
+++ proyecto_Interfaces/src/app/series/series.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { navbarModule } from '../navbar/navbar.module';
+
+
+import { footerModule } from '../footer/footer.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { Ng2SearchPipeModule} from 'ng2-search-filter';
+import { seriesComponent } from './series.component';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    seriesComponent, 
+   
+       ],
+  imports: [
+    CommonModule,
+    navbarModule,
+    footerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule
+    
+    
+  ],
+ 
+})
+export class seriesModule { }
