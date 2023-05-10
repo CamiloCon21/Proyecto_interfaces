e5903378e27e44e34898598401f60e6300cbafd7
JIsaacVG <104176710+JIsaacVG@users.noreply.github.com>
2023-05-10 13:07:32 -0500

    creacion personajes

--- proyecto_Interfaces/src/app/character/character.module.ts
+++ proyecto_Interfaces/src/app/character/character.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { navbarModule } from '../navbar/navbar.module';
 
-
+import { characterComponent } from './character.component';
 import { footerModule } from '../footer/footer.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { Ng2SearchPipeModule} from 'ng2-search-filter';
-import { characterComponent } from './character.component';
+
 
 
 
