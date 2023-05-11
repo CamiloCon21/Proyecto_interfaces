31a22287f1e68727472785d7c37f5b35c8fd4b82
JIsaacVG <104176710+JIsaacVG@users.noreply.github.com>
2023-05-11 12:06:43 -0500

    mejora login

--- proyecto_Interfaces/src/app/Login/Login.component.ts
+++ proyecto_Interfaces/src/app/Login/Login.component.ts
@@ -1,6 +1,7 @@
+import { Router, RouterLink } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
-import { Router, RouterLink } from '@angular/router';
+
 
 
 @Component({
