b531812561f1ec550b4f6437bead29ed87a6c468
JIsaacVG <104176710+JIsaacVG@users.noreply.github.com>
2023-05-13 12:46:52 -0500

    actualizacion navbar

--- proyecto_Interfaces/src/app/navbar/navbar.module.ts
+++ proyecto_Interfaces/src/app/navbar/navbar.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { navbarRoutingModule } from './navbar-routingmodule';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 
 
